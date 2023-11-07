dd3fe2b6a22ea8ba6a1681fe221dfb61f6dd413c
YoonsGitIt <wldbs2385@gmail.com>
2023-11-07 20:36:47 +0900

    set trajectory and time step

--- examples/src/kinova.cpp
+++ examples/src/kinova.cpp
@@ -2,6 +2,7 @@
 // Created by jy on 23. 11. 1.
 //
 
+#include <unistd.h>
 #include "raisim/RaisimServer.hpp"
 #include "raisim/World.hpp"
 #include "cubicTrajectoryGenerator.hpp"
@@ -18,62 +19,71 @@ int main(int argc, char* argv[]) {
     world.addGround();
     auto kinova = world.addArticulatedSystem("/home/tina/EE3100704/examples/rsc/kinova/urdf/kinova.urdf");
 
+    /// launch raisim server
+    raisim::RaisimServer server(&world);
+    server.setMap("simple");
+    server.launchServer();
+
+    server.focusOn(kinova);
+    kinova->setName("kinova");
+
     /// set time
     setTime setTime;
     setTime.setTimeInitiallize();
     setTime.timedT = 0.02;
 
-    /// create trajectory
-    cubicTrajectoryGenerator trajectoryGenerator[kinova->getDOF()];
-    double timeDuration = 1.0;
-    Eigen::VectorXd jointGoalPosition = Eigen::VectorXd (kinova->getDOF());
-    Eigen::VectorXd jointCurrentPosition = Eigen::VectorXd (kinova->getDOF());
-
-    jointGoalPosition << 0.0, 0.0, 0.0, 0.0, 2.0, 0.0;
-    jointCurrentPosition << 0.0, 2.76, -1.57, 0.0, 2.0, 0.0;
-    for (int i = 0; i < kinova->getDOF(); i++)
-    {
-        trajectoryGenerator[i].updateTrajectory(jointCurrentPosition[i],jointGoalPosition[i],setTime.localtime,timeDuration)  ;
-    }
-
     /// kinova joint PD controller
-    Eigen::VectorXd jointNominalConfig(kinova->getGeneralizedCoordinateDim()), jointVelocityTarget(kinova->getDOF());
-    jointNominalConfig << 0.0, 2.76, -1.57, 0.0, 2.0, 0.0;
-    jointVelocityTarget.setZero();
-
+    Eigen::VectorXd jointPositionTarget(kinova->getGeneralizedCoordinateDim()), jointVelocityTarget(kinova->getDOF());
     Eigen::VectorXd jointPgain(kinova->getDOF()), jointDgain(kinova->getDOF());
     jointPgain << 40.0, 40.0, 40.0, 15.0, 15.0, 15.0;
-    jointDgain << 1.0, 1.0, 1.0, 0.5, 0.5, 0.5;
+    jointDgain << 2.0, 2.0, 2.0, 0.5, 0.5, 0.5;
+
+    ///set joint initial state
+    cubicTrajectoryGenerator trajectoryGenerator[kinova->getDOF()];
+    double timeDuration = 3.0;
+    Eigen::VectorXd jointGoalPosition(kinova->getGeneralizedCoordinateDim());
+    Eigen::VectorXd initialJointPosition(kinova->getGeneralizedCoordinateDim());
+    initialJointPosition.setZero();
 
     kinova->setPdGains(jointPgain, jointDgain);
-    kinova->setPdTarget(jointNominalConfig, jointVelocityTarget);
+    kinova->setPdTarget(jointPositionTarget, jointVelocityTarget);
+    kinova->setGeneralizedCoordinate(initialJointPosition);
+    sleep(5);
+
+    /// set joint goal position
+
+    jointGoalPosition << 0.0, 3.141592, -1.57, 0.0, 2.0, 0.0;
+
+    std::cout <<  kinova->getGeneralizedCoordinate() << std::endl;
+
+    /// create trajectory
+    for (int i = 0; i < kinova->getDOF(); i++)
+    {
+        trajectoryGenerator[i].updateTrajectory(initialJointPosition[i],jointGoalPosition[i],setTime.localtime,timeDuration)  ;
+    }
 
     while (1)
     {
         setTime.setLocaltime(); //get in while loop.
         for (int jointNum = 0; jointNum < kinova->getDOF() ; jointNum++)
         {
-            jointNominalConfig[jointNum] = trajectoryGenerator[jointNum].getPositionTrajectory(setTime.localtime);
+            jointPositionTarget[jointNum] = trajectoryGenerator[jointNum].getPositionTrajectory(setTime.localtime);
             jointVelocityTarget[jointNum] = trajectoryGenerator[jointNum].getVelocityTrajectory(setTime.localtime);
         }
 
-        std::cout << jointNominalConfig << std::endl;
-
-        /// kinova set torque
-        kinova->setGeneralizedCoordinate(jointNominalConfig);
-//        kinova->setGeneralizedForce(Eigen::VectorXd::Zero(kinova->getDOF()));
+        /// kinova set position
+        kinova->setGeneralizedCoordinate(jointPositionTarget);
+        usleep(10000);
+        std::cout << kinova->getGeneralizedCoordinate() << std::endl;
         if (setTime.localtime == timeDuration)
             break;
     }
 
+    kinova->setPdGains(jointPgain, jointDgain);
+    kinova->setPdTarget(jointPositionTarget, jointVelocityTarget);
 
-    kinova->setName("kinova");
+    std::cout << kinova->getGeneralizedCoordinate() << std::endl;
 
-    /// launch raisim server
-    raisim::RaisimServer server(&world);
-    server.setMap("simple");
-    server.launchServer();
-    server.focusOn(kinova);
 
     for (int i=0; i<2000000; i++) {
         RS_TIMED_LOOP(int(world.getTimeStep()*1e6))
